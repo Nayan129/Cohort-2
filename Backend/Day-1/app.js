e898860ec420ae10eccbef98786c44e3ee813c52
Nayan <nayanbhusari02@gmail.com>
2026-01-21 17:19:21 +0530

    Day-1 backend files

---
+++ Backend/Day-1/app.js
@@ -0,0 +1,6 @@
+// we required express package and store it inside express variable
+const express = require("express");
+// we store express inside app
+const app = express();
+// running app on a particular port i.e. 3000
+app.listen(3000);
