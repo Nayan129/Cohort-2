fa66d696d6bb153ed685c4aa3208ec919d3de6c8
Nayan <nayanbhusari02@gmail.com>
2026-01-24 10:47:11 +0530

    Update Day-2 server.js

--- Backend/Day-2/server.js
+++ Backend/Day-2/server.js
@@ -5,10 +5,14 @@ const app = express(); // server instance created here
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.get("/about",(req,res)=>{
-  res.send("this is about page")
-})
+app.get("/about", (req, res) => {
+  res.send("this is about page");
+});
+app.get("/home", (req, res) => {
+  res.send("this is home page");
+});
 
-app.listen(3000, () => { // server start here 
+app.listen(3000, () => {
+  // server start here
   console.log("server started at port 3000 successfully...");
 });
