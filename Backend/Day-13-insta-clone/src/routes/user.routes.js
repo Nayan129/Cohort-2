d6e0a13f662b3646a3623aee05a25b00079be472
Nayan <nayanbhusari02@gmail.com>
2026-02-13 19:09:44 +0530

    controller folder

--- Backend/Day-13-insta-clone/src/routes/user.routes.js
+++ Backend/Day-13-insta-clone/src/routes/user.routes.js
@@ -1,108 +1,11 @@
 const express = require("express");
-const userModel = require("../models/user.model");
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
+const authController = require("../controllers/authController");
 const authRouter = express.Router();
 
 // here is user register api/route
-authRouter.post("/register", async (req, res) => {
-  const { username, email, password, bio, profileImage } = req.body;
-
-  // here we use signle calling so that at single call to database and check username or email exist
-  const isUserAlreadyExist = await userModel.findOne({
-    $or: [{ username }, { email }],
-  });
-
-  if (isUserAlreadyExist) {
-    return res.status(409).json({
-      message:
-        "user already exist" +
-        (isUserAlreadyExist.email == email
-          ? "email already exist choose different email"
-          : "username already exist choose different username"),
-    });
-  }
-
-  // we save password in hash format so that if database leak password still secure
-  const hash = crypto.createHash("sha256").update(password).digest("hex");
-
-  // now time to create user using userModel.create
-  const user = await userModel.create({
-    username,
-    email,
-    bio,
-    profileImage,
-    password: hash,
-  });
-
-  // now we create token that with store user after creation
-  const token = jwt.sign(
-    {
-      id: user._id,
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" },
-  );
-
-  // now we store that cookie in res.cookie
-  res.cookie("token", token);
-
-  // now we send respnose as "201" user created successfully
-  res.status(201).json({
-    message: "user created successfully",
-    user: {
-      username: user.username,
-      email: user.email,
-      bio: user.bio,
-      profileImage: user.profileImage,
-    },
-  });
-});
+authRouter.post("/register", authController.registerController);
 
 // here is user login api.route
-authRouter.post("/login", async (req, res) => {
-  const { username, email, password } = req.body;
-
-  const user = await userModel.findOne({
-    $or: [{ username: username }, { email: email }],
-  });
-
-  if (!user) {
-    return res.status(404).json({
-      message: "user not found",
-    });
-  }
-
-  const hash = crypto.createHash("sha256").update(password).digest("hex");
-
-  const isPasswordValid = hash == user.password;
-  if (!isPasswordValid) {
-    return res.status(401).json({
-      message: "invalid password",
-    });
-  }
-
-  const token = jwt.sign(
-    {
-      id: user._id,
-    },
-    process.env.JWT_SECRET,
-  );
-
-  res.cookie("token", token);
-
-  res.status(200).json({
-    message: "user loggedIn successfully",
-    user: {
-      username: user.username,
-      email: user.email,
-      bio: user.bio,
-      profileImage: user.profileImage,
-    },
-  });
-
-  
-});
-
+authRouter.post("/login", authController.loginController);
 
 module.exports = authRouter;
