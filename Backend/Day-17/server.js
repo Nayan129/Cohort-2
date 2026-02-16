5662e7c6aa0e7dff25c121f7adde3eb4268f0447
Nayan <nayanbhusari02@gmail.com>
2026-02-16 22:42:59 +0530

    Add dotenv configuration and database connection setup

--- Backend/Day-17/server.js
+++ Backend/Day-17/server.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
 const app = require("./src/app");
+const connectToDB = require("./src/config/database");
+
+connectToDB();
 
 app.listen(3000, () => {
   console.log("server running on port 3000 successfully...");
