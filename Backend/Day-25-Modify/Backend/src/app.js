d2bffa6be927d86714b6910a8fb5922f253691ed
Nayan <nayanbhusari02@gmail.com>
2026-03-04 11:55:13 +0530

    state layer done

--- Backend/Day-25-Modify/Backend/src/app.js
+++ Backend/Day-25-Modify/Backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors",{
+  
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
 
 /**
  * Routes import and use here..
--- Backend/Day-25-Modify/Frontend/src/App.jsx
+++ Backend/Day-25-Modify/Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./features/shared/global.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app.routes";
-
+import { AuthProvider } from "./features/auth/auth.context";
 const App = () => {
   return (
     <div className="app">
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   );
 };
