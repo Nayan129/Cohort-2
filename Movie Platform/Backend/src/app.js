b35a8162f139374bf058beba5b59f98dcd87c247
Nayan <nayanbhusari02@gmail.com>
2026-03-09 01:08:57 +0530

    frotnend integration we starting now

--- Movie Platform/Backend/src/app.js
+++ Movie Platform/Backend/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({}));
 
 /**
  * Routes import and use here..
