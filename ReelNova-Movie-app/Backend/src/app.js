468c4d3e72105999fe7a8db5163db2603d8fe4e8
Nayan <nayanbhusari02@gmail.com>
2026-03-12 09:38:04 +0530

    cors policy added

--- ReelNova-Movie-app/Backend/src/app.js
+++ ReelNova-Movie-app/Backend/src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://reelnova-movie-app.netlify.app",
     credentials: true,
   }),
 );
