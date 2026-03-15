aee11217b217f33ea6430e14ca988b7bc9e8765a
Nayan <nayanbhusari02@gmail.com>
2026-03-15 20:40:36 +0530

    socket io

---
+++ Backend/Day-31 Socket.io/server.js
@@ -0,0 +1,14 @@
+import app from "./src/app.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
+
+io.on("connection", (Socket) => {
+  console.log("new connection created");
+});
+
+httpServer.listen(3000, () => {
+  console.log("server running on port 3000");
+});
