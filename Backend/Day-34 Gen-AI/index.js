2b14e2cfae2288bfb84627c293dc00efaf0c52b8
Nayan <nayanbhusari02@gmail.com>
2026-03-17 17:07:38 +0530

    to send email nodemailer install

--- Backend/Day-34 Gen-AI/index.js
+++ Backend/Day-34 Gen-AI/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
 import { HumanMessage } from "langchain";
+import { sendEmail } from "./mail.service.js";
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -19,7 +20,9 @@ while (true) {
 
   messages.push(new HumanMessage(userInput));
 
-  const response = await model.invoke(userInput);
+  const response = await model.invoke(messages);
+
+  messages.push(response);
 
   console.log(`\x1b[34m[AI]\x1b[0m ${response.content}`);
 }
