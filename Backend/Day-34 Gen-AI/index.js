9af777b69f73528ac56c8bfcb9cb4338c7393629
Nayan <nayanbhusari02@gmail.com>
2026-03-17 16:41:23 +0530

    ai implement

--- Backend/Day-34 Gen-AI/index.js
+++ Backend/Day-34 Gen-AI/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
+import { HumanMessage } from "langchain";
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,14 +12,16 @@ const model = new ChatMistralAI({
   model: "mistral-small-latest",
 });
 
+const messages = [];
+
 while (true) {
   const userInput = await rl.question("\x1b[32myou:\x1b[0m ");
+
+  messages.push(new HumanMessage(userInput));
 
   const response = await model.invoke(userInput);
 
   console.log(`\x1b[34m[AI]\x1b[0m ${response.content}`);
-
-
 }
 
 rl.close();
