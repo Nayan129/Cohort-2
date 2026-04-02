f886f260677b3b9e8192527c26f6d7162c9a4b49
Nayan <nayanbhusari02@gmail.com>
2026-04-02 13:28:32 +0530

    implemented first typescript code

---
+++ Typescript/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "Nayan";
+let age = 22;
+let isActive = true;
+console.log(name, age, isActive);
+//# sourceMappingURL=index.js.map
---
+++ Typescript/index.ts
@@ -0,0 +1,5 @@
+let name: string = "Nayan";
+let age: number = 22;
+let isActive: boolean = true;
+
+console.log(name, age, isActive);
