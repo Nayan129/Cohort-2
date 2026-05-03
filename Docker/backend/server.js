7b35ee50982ab9650d2cacb3ff0b818fba2b5e3b
Nayan <nayanbhusari02@gmail.com>
2026-05-03 14:47:02 +0530

    Add initial frontend setup with React, Vite, and styling
    - Created main entry point for React application in `main.jsx`
    - Configured Vite for development with proxy settings in `vite.config.js`
    - Added global styles in `index.css` with light and dark mode support
    - Included SVG assets for React and Vite logos

---
+++ Docker/backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+  });
+});
+
+app.get("/api/hello", (req, res) => {
+  res.status(200).json({
+    message: "Hello World",
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  const users = [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "brandon" },
+    { id: 3, name: "Roy" },
+  ];
+  res.status(200).json(users);
+});
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
